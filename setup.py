24bbe894b48b9fd14b7be801afdf433cb1944428
hwu84 <hwu84@wisc.edu>
2016-12-11 15:13:01 -0600

    add readme file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lightchem',
+      version='0.1',
+      description='Light-weight Machine Learning models for Drug Discovery',
+      url='https://github.com/haozhenWu/lightchem',
+      author='Haozhen Wu',
+      author_email='wuhaozhen@hotmail.com',
+      license='N/A',
+      packages=['lightchem'],
+      zip_safe=False)
