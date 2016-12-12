4a1ef2d31474f21c9e90f875d2cb774aca4c5d8d
hwu84 <hwu84@wisc.edu>
2016-12-12 15:47:38 -0600

    add fingerprint generation script for muv

--- datasets/muv/classification/muv_fingerprint.py
+++ datasets/muv/classification/muv_fingerprint.py
@@ -7,10 +7,10 @@ import os
 if __name__ == "__main__":
 
     current_dir = os.path.dirname(os.path.realpath(__file__))
-    muv = pd.read_csv(current_dir + "deepchem_muv.csv.gz")
+    muv = pd.read_csv(current_dir + "/deepchem_muv.csv.gz")
     # get target names
     muv_targetName = pd.DataFrame(muv.columns[0:17])
-    muv_targetName.to_csv(current_dir + "../muv_TargetName.csv",index=False, header = False)
+    muv_targetName.to_csv(current_dir + "/../muv_TargetName.csv",index=False, header = False)
 
     # Assign 0 to missing label.
     muv = muv.fillna(0)
@@ -19,9 +19,9 @@ if __name__ == "__main__":
     fp = fingerprint.smile_to_fps(muv,smile_colname)
     # morgan(ecfp) fp
     morgan_fp = fp.Morgan()
-    morgan_fp.to_csv(current_dir + "muv_BinaryLabel_ecfp1024.csv",index = False)
+    morgan_fp.to_csv(current_dir + "/muv_BinaryLabel_ecfp1024.csv",index = False)
 
     # MACCSkeys fp
     fp = fingerprint.smile_to_fps(tox21,smile_colname)
     maccs_fp = fp.MACCSkeys()
-    maccs_fp.to_csv(current_dir + "muv_BinaryLabel_MACCSkey167.csv",index = False)
+    maccs_fp.to_csv(current_dir + "/muv_BinaryLabel_MACCSkey167.csv",index = False)
