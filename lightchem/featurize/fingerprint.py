35039dcc320fb69dff8bbdcd46ea600374e0c8ba
hwu84 <hwu84@wisc.edu>
2016-12-11 19:39:19 -0600

    initial version of fingerprint class

--- lightchem/featurize/fingerprint.py
+++ lightchem/featurize/fingerprint.py
@@ -1,17 +1,11 @@
+"""
+Wrapper of rdkit package to transform molucule into fingerprint.
+"""
 from rdkit import Chem
 from rdkit.Chem.Fingerprints import FingerprintMols
 from rdkit.Chem import AllChem
 from rdkit.Chem import MACCSkeys
 import pandas as pd
-
-base_data_dir = '/Users/alan/Desktop/study/SmallMolecular/pcba128_multitask_data/'
-binaryLabel_dir = 'pcba128_mmtn_canon_ecfp1024.csv'
-df = pd.read_csv(base_data_dir + binaryLabel_dir)
-df = pd.read_csv('/home/haozhen/Haozhen-data/pcba128_python/data/pcba128_canon_ecfp1024_logac50.csv')
-
-# input DataFrame contains col 'smile'
-result_df = df.loc[:,['mol_id','smiles']]
-
 
 class smile_to_fps(object):
 
@@ -32,7 +26,23 @@ class smile_to_fps(object):
                 print i
             else:
                 k = k+1
-                print("fail")
+        print 'Number of molecue failed: ' + str(k)
+        return self.__df
+
+    def MACCSkeys(self):
+        k = 0
+        for i,smile in enumerate(result_df.smiles):
+            if Chem.MolFromSmiles(smile):
+                tmp_mol = Chem.MolFromSmiles(smile)
+                fps = MACCSkeys.GenMACCSKeys(tmp_mol)
+                self.__df.fingerprint[i] = fps.ToBitString()
+                print i
+            else:
+                k += 1
+        print 'Number of molecue failed: ' + str(k)
+        return self.__df
+
+""" RDKit featurization method has problem. Create unequal length fingerprint.
 
     def RDKit(self):
         k = 0
@@ -46,50 +56,4 @@ class smile_to_fps(object):
                 k = k+1
                 print("fail")
 
-    def MACCSkeys(self):
-        k = 0
-        for i,smile in enumerate(result_df.smiles):
-            if Chem.MolFromSmiles(smile):
-                tmp_mol = Chem.MolFromSmiles(smile)
-                fps = MACCSkeys.GenMACCSKeys(tmp_mol)
-                result_df.fingerprint[i] = fps.ToBitString()
-                print i
-            else:
-                k = k+1
-                print("fail")
-
-
-
-
-
-from rdkit import Chem
-from rdkit.Chem.Fingerprints import FingerprintMols
-
-result_df['fingerprint'] = "none"
-k = 0
-for i,smile in enumerate(result_df.smiles):
-    if Chem.MolFromSmiles(smile):
-        tmp_mol = Chem.MolFromSmiles(smile)
-        fps = FingerprintMols.FingerprintMol(tmp_mol)
-        result_df.fingerprint[i] = fps.ToBitString()
-        print i
-    else:
-        k = k+1
-        print("fail")
-
-
-from rdkit.Chem import MACCSkeys
-
-result_df['fingerprint'] = "none"
-k = 0
-for i,smile in enumerate(result_df.smiles):
-    if Chem.MolFromSmiles(smile):
-        tmp_mol = Chem.MolFromSmiles(smile)
-        fps = MACCSkeys.GenMACCSKeys(tmp_mol)
-        result_df.fingerprint[i] = fps.ToBitString()
-        print i
-    else:
-        k = k+1
-        print("fail")
-
-df.to_csv("./pcba128_multitask_data/pcba128_mmtn_canon_MACCSkeys167.csv",index=False")
+"""
