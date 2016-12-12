df2b9ecddfc3f52fd26d79bc8f06bd74b561144e
hwu84 <hwu84@wisc.edu>
2016-12-12 13:28:33 -0600

    improve tox21 example

--- example/tox21/gather_tox21_result.py
+++ example/tox21/gather_tox21_result.py
@@ -9,11 +9,13 @@ all_files = glob.glob( dir_to_store_result + 'each_target_cv_result/' + dataset_
 base = pd.read_csv(all_files[0])
 cv_result = pd.DataFrame(base.iloc[:,1])
 cv_result.index = base.iloc[:,0]
+cv_result.index.name = 'model'
 
 for i,file in enumerate(all_files[1:len(all_files)]):
     temp = pd.read_csv(file)
     temp2 = pd.DataFrame(temp.iloc[:,1])
     temp2.index = temp.iloc[:,0]
+    temp2.index.name = 'model'
     cv_result = pd.concat([cv_result,temp2],axis = 1)
 
 cv_result.to_csv(dir_to_store_result + dataset_name + "_cv_result.csv")
@@ -26,6 +28,7 @@ target_name = re.sub('_test_result.csv','',target_name)
 # reconstruct dataframe for easy to view
 test_result = pd.DataFrame({target_name : list(base.iloc[0:6,2])+list(base.iloc[6:12,1])})
 test_result.index = base.iloc[:,0]
+test_result.index.name = 'model'
 test_result = test_result.iloc[[0,3,1,2,6,9,7,8]]
 
 for i,file in enumerate(all_files[1:len(all_files)]):
@@ -34,6 +37,7 @@ for i,file in enumerate(all_files[1:len(all_files)]):
     target_name = re.sub('_test_result.csv','',target_name)
     temp2 = pd.DataFrame({target_name : list(temp.iloc[0:6,2])+list(temp.iloc[6:12,1])})
     temp2.index = temp.iloc[:,0]
+    temp2.index.name = 'model'
     temp2 = temp2.iloc[[0,3,1,2,6,9,7,8]]
     test_result = pd.concat([test_result,temp2],axis = 1)
 
