1461401e4199db98b391fbe13f9ce79ea817fba4
hwu84 <hwu84@wisc.edu>
2016-12-12 15:48:39 -0600

    fix bug in tox21 fingerprint generation

--- datasets/tox21/classification/tox21_fingerprint.py
+++ datasets/tox21/classification/tox21_fingerprint.py
@@ -7,10 +7,10 @@ import os
 if __name__ == "__main__":
 
     current_dir = os.path.dirname(os.path.realpath(__file__))
-    tox21 = pd.read_csv(current_dir + "deepchem_tox21.csv.gz")
+    tox21 = pd.read_csv(current_dir + "/deepchem_tox21.csv.gz")
     # get target names
     tox21_targetName = pd.DataFrame(tox21.columns[0:11])
-    tox21_targetName.to_csv(current_dir + "../tox21_TargetName.csv",index=False, header = False)
+    tox21_targetName.to_csv(current_dir + "/../tox21_TargetName.csv",index=False, header = False)
 
     # Assign 0 to missing value.
     tox21 = tox21.fillna(0)
@@ -19,9 +19,9 @@ if __name__ == "__main__":
     fp = fingerprint.smile_to_fps(tox21,smile_colname)
     # morgan(ecfp) fp
     morgan_fp = fp.Morgan()
-    morgan_fp.to_csv(current_dir + "tox21_BinaryLabel_ecfp1024.csv",index = False)
+    morgan_fp.to_csv(current_dir + "/tox21_BinaryLabel_ecfp1024.csv",index = False)
 
     # MACCSkeys fp
     fp = fingerprint.smile_to_fps(tox21,smile_colname)
     maccs_fp = fp.MACCSkeys()
-    maccs_fp.to_csv(current_dir + "tox21_BinaryLabel_MACCSkey167.csv",index = False)
+    maccs_fp.to_csv(current_dir + "/tox21_BinaryLabel_MACCSkey167.csv",index = False)
