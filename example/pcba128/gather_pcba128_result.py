dae2c521f92015c0ab991f5fe3ff675eeba39967
hwu84 <hwu84@wisc.edu>
2016-12-12 15:27:10 -0600

    update pcba128 example

--- example/pcba128/gather_pcba128_result.py
+++ example/pcba128/gather_pcba128_result.py
@@ -8,11 +8,13 @@ all_files = glob.glob( dir_to_store_result + 'each_target_cv_result/' + "aid*_cv
 base = pd.read_csv(all_files[0])
 cv_result = pd.DataFrame(base.iloc[:,1])
 cv_result.index = base.iloc[:,0]
+cv_result.index.name = 'model'
 
 for i,file in enumerate(all_files[1:len(all_files)]):
     temp = pd.read_csv(file)
     temp2 = pd.DataFrame(temp.iloc[:,1])
     temp2.index = temp.iloc[:,0]
+    temp2.index.name = 'model'
     cv_result = pd.concat([cv_result,temp2],axis = 1)
 
 cv_result.to_csv(dir_to_store_result + "pcba128_cv_result.csv")
@@ -24,6 +26,7 @@ aid = re.findall('aid\d{1,10}',all_files[0])[0]
 # reconstruct dataframe for easy to view
 test_result = pd.DataFrame({aid : list(base.iloc[0:10,2])+list(base.iloc[10:20,1])})
 test_result.index = base.iloc[:,0]
+test_result.index.name = 'model'
 test_result = test_result.iloc[[0,5,1,2,3,4,10,15,11,12,13,14]]
 
 for i,file in enumerate(all_files[1:len(all_files)]):
@@ -31,6 +34,7 @@ for i,file in enumerate(all_files[1:len(all_files)]):
     aid = re.findall('aid\d{1,10}',file)[0]
     temp2 = pd.DataFrame({aid : list(temp.iloc[0:10,2])+list(temp.iloc[10:20,1])})
     temp2.index = temp.iloc[:,0]
+    temp2.index.name = 'model'
     temp2 = temp2.iloc[[0,5,1,2,3,4,10,15,11,12,13,14]]
     test_result = pd.concat([test_result,temp2],axis = 1)
 
