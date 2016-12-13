e3144253cdf9f9a4a11f08831a0be2934bc13616
hwu84 <hwu84@wisc.edu>
2016-12-13 12:51:09 -0600

    remove old scripts

--- example/demo3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# input argument: aid_list.csv
-while IFS='' read -r line;do
-
-/home/haozhen/anaconda2/bin/python /home/haozhen/Haozhen-data/lightchem/example/demo3.py $line
-
-done <"$1"
