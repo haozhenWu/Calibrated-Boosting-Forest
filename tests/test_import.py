de34f6bb9075b3a6fe4522885d773ae82802f4de
Anthony Gitter <gitter@biostat.wisc.edu>
2016-12-22 06:25:26 -0600

    Add test function to test that dependencies can be imported

---
+++ tests/test_import.py
@@ -0,0 +1,14 @@
+'''
+Test that all dependencies can be imported
+'''
+
+import xgboost
+import sklearn
+import lightchem
+
+def test_import():
+    '''
+    The function does nothing, the test is whether the import statements
+    work.
+    '''
+    assert True
