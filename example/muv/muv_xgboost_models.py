b71f2c449b0bc9eaca094199d87bd61d0590a97d
Anthony Gitter <gitter@biostat.wisc.edu>
2016-12-23 08:15:40 -0600

    Add output to MUV example code to sustain Travis CI testing

--- example/muv/muv_xgboost_models.py
+++ example/muv/muv_xgboost_models.py
@@ -39,6 +39,7 @@ if __name__ == "__main__":
     start_time = time.time()
     SEED = 2016
     #----------------------------------- Build first layer data
+    print '{}: loading data'.format(target_name)
     current_dir = os.path.dirname(os.path.realpath(__file__))
     setting_list = []
     # binary ecfp1024
@@ -77,6 +78,7 @@ if __name__ == "__main__":
     #---------------------------------first layer models ----------
     # 4 layer1 models based on ecfp,MACCSkeys data
     # gbtree
+    print '{}: building first layer models'.format(target_name)
     layer1_model_list = []
     evaluation_metric_name = 'ROCAUC'
     for data_dict in setting_list:
@@ -105,6 +107,7 @@ if __name__ == "__main__":
     #------------------------------------second layer models
     # use label from binary data to train layer2 models
     #layer1_model_list
+    print '{}: building second layer models'.format(target_name)
     layer2_label_data = setting_list[0]['data'] # layer1 data object containing the label for layer2 model
     layer2_model_list = []
     layer2_modeltype = ['GbtreeLogistic','GblinearLogistic']
@@ -134,6 +137,7 @@ if __name__ == "__main__":
 
     #------------------------------------ evaluate model performance on test data
     # prepare test data, retrive from layer1 data
+    print '{}: evaluating test set'.format(target_name)
     list_TestData = []
     for data_dict in setting_list:
         for model_type in data_dict['model_type']:
@@ -168,6 +172,7 @@ if __name__ == "__main__":
     # collect test result
     result = pd.concat(test_result_list,axis = 0,ignore_index=False)
     result.to_csv(os.path.join(dir_to_store_result, 'each_target_test_result', dataset_name + '_' + target_name + "_test_result.csv"))
+    print '{}: finished'.format(target_name)
 
     # moniter processing time
     with open(os.path.join(dir_to_store_result, "process_time.txt"), "a") as text_file:
