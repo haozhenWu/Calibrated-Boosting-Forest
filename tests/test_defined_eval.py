5637bf1ab68fc6269d2cd638c8fb146487e90031
hwu84 <hwu84@wisc.edu>
2017-02-17 09:34:30 -0600

    add unit test for defined_eval.py

---
+++ tests/test_defined_eval.py
@@ -0,0 +1,14 @@
+"""
+Unit-test for defined_eval
+"""
+from lightchem.eval import defined_eval
+
+def test_defined_eval():
+    defined_eval = defined_eval.definedEvaluation()
+    assert defined_eval.is_maximize('ROCAUC') == True
+    assert defined_eval.stopping_round('ROCAUC') == 100
+    try:
+        defind_eval.is_maximize('not_exist_eval_name')
+        assert mark == 1
+    except ValueError:
+        mark = 1
