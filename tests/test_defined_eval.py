d143b2ad094081aa2ff9da381aa38cb09199fe27
hwu84 <hwu84@wisc.edu>
2017-02-18 02:36:16 -0600

    assign value to a variable before reference it

--- tests/test_defined_eval.py
+++ tests/test_defined_eval.py
@@ -7,8 +7,8 @@ def test_defined_eval():
     defined_eval = defined_eval.definedEvaluation()
     assert defined_eval.is_maximize('ROCAUC') == True
     assert defined_eval.stopping_round('ROCAUC') == 100
-    mark = 0
     try:
+        mark = 0
         defind_eval.is_maximize('not_exist_eval_name')
         assert mark == 1
     except ValueError:
