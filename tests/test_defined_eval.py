df78562a64baffb389f1842ba7fb206e103ac09d
hwu84 <hwu84@wisc.edu>
2017-02-18 14:11:37 -0600

    variable name should be different from class name

--- tests/test_defined_eval.py
+++ tests/test_defined_eval.py
@@ -4,12 +4,12 @@ Unit-test for defined_eval
 from lightchem.eval import defined_eval
 
 def test_defined_eval():
-    defined_eval = defined_eval.definedEvaluation()
-    assert defined_eval.is_maximize('ROCAUC') == True
-    assert defined_eval.stopping_round('ROCAUC') == 100
+    eval = defined_eval.definedEvaluation()
+    assert eval.is_maximize('ROCAUC') == True
+    assert eval.stopping_round('ROCAUC') == 100
+    mark = 0
     try:
-        mark = 0
-        defind_eval.is_maximize('not_exist_eval_name')
+        eval.is_maximize('not_exist_eval_name')
         assert mark == 1
     except ValueError:
         mark = 1
