f52c139e1077fb5f04dd48e668edc8cc5ecf7dd0
hwu84 <hwu84@wisc.edu>
2017-02-27 09:48:46 -0600

    add evalname

--- lightchem/wrapper/VS_wrapper.py
+++ lightchem/wrapper/VS_wrapper.py
@@ -53,6 +53,9 @@ if __name__ == "__main__":
     dir = "./datasets/muv/classification/deepchem_muv.csv.gz" # arg1
     smile_colname = 'smiles' # arg2
     label_name_list = ['MUV-466'] # arg3
+    eval_name = 'ROCAUC' # arg4
+    preDefined_eval = defined_eval.definedEvaluation()
+    preDefined_eval.validate_eval_name(eval_name)    
     df = pd.read_csv(dir)
     # identify NA row.
     missing_row = pd.isnull(df.loc[:,label_name_list[0]])
