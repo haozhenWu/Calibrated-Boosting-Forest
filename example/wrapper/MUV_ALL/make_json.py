04a5d38b8a9849179ca79e53062397c489793517
hwu84 <hwu84@wisc.edu>
2017-03-03 09:39:33 -0600

    minor update

--- example/wrapper/MUV_ALL/make_json.py
+++ example/wrapper/MUV_ALL/make_json.py
@@ -11,8 +11,10 @@ if __name__ == "__main__":
     with open(sys.argv[1], 'r') as f:
         info = f.read()
     json = pd.read_json(info)
-
-    json.loc['target_name'] = sys.argv[2]
-    json.loc['label_name_list'] = [sys.argv[2]]
+    name = sys.argv[2]
+    json.loc['target_name'] = name
+    name_list = []
+    name_list.append(name)
+    json.loc['label_name_list'] = name_list
 
     json.to_json("./config.json")
