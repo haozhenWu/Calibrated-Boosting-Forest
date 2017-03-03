915bb1ebbb03248eb047005b11fae2d7ddd9faf5
hwu84 <hwu84@wisc.edu>
2017-03-03 09:36:16 -0600

    run path

--- example/wrapper/MUV_ALL/run.sh
+++ example/wrapper/MUV_ALL/run.sh
@@ -6,4 +6,4 @@ echo Running $line
 /home/haozhen/anaconda2/bin/python 'make_json.py' ./config.json $line
 /home/haozhen/anaconda2/bin/python ../../../wrapper/VS_wrapper.py ./config.json
 
-done <"$1"     
+done <"$1"      
