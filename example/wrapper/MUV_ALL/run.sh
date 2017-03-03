076baf8cf950c621d54885a98078f8fd01bb1019
hwu84 <hwu84@wisc.edu>
2017-03-03 09:22:10 -0600

    typo

--- example/wrapper/MUV_ALL/run.sh
+++ example/wrapper/MUV_ALL/run.sh
@@ -3,5 +3,5 @@
 while IFS='' read -r line;do
 
 echo Running $line
-/home/haozhen/anaconda2/bin/python 'make_json.py' $line $1
+/home/haozhen/anaconda2/bin/python 'make_json.py' ./config.json $line 
 /home/haozhen/anaconda2/bin/python ../../../wrapper/VS_wrapper.py ./config.json
