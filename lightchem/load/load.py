56463a6de81b067ff9204609f215bcbc0ba51c9e
haozhenWu <hwu84@wisc.edu>
2017-04-25 22:23:24 -0500

    minor update

--- lightchem/load/load.py
+++ lightchem/load/load.py
@@ -11,7 +11,7 @@ class readData(object):
         Class to read data,such as fingerprint stored as string in one column
         or column names starting with `Feature_`, and transform to ndarray.
         '''
-        def __init__(self,data_loc,label_name):
+        def __init__(self,data_loc,label_name=None):
             """
             Parameters:
             -----------
@@ -27,8 +27,9 @@ class readData(object):
             elif isinstance(data_loc,str):
                 self.__data_pd = None
                 self.__file_path = data_loc
-            #self.__file_path = file_path
-            assert isinstance(label_name,str)
+            # Testing data does not have label_name 
+            if label_name is not None:
+                assert isinstance(label_name,str)
             self.__label_name = label_name
             self.__X_data = None
             self.__y_data = None
@@ -38,21 +39,20 @@ class readData(object):
             """
             if not isinstance(self.__data_pd,pd.DataFrame):
                 self.__data_pd = pd.read_csv(self.__file_path)
-            #data_pd = pd.read_csv(self.__file_path)
-            self.__y_data = self.__data_pd[self.__label_name]
-            self.__y_data = self.__y_data.astype(np.float64)
+            if self.__label_name is not None:
+                self.__y_data = self.__data_pd[self.__label_name]
+                self.__y_data = self.__y_data.astype(np.float64)
+                self.__y_data = np.array(self.__y_data)
+                self.__y_data = self.__y_data.astype(np.float64)
+
             # extracting features from DataFrame
             if 'fingerprint' in self.__data_pd.columns:
                 self.__X_data = util.fpString_to_array(self.__data_pd['fingerprint'])
-                self.__y_data = np.array(self.__y_data)
-                self.__y_data = self.__y_data.astype(np.float64)
             else: #havnt test below else code. just write a frame first
                 features_cols = [ col for col in self.__data_pd.columns if 'Feature_' in col]
                 self.__X_data = self.__data_pd[features_cols]
                 self.__X_data = np.array(self.__X_data)
                 self.__X_data = self.__X_data.astype(np.float64)
-                self.__y_data = np.array(self.__y_data)
-                self.__y_data = self.__y_data.astype(np.float64)
 
         def features(self):
             """
@@ -70,6 +70,7 @@ class readData(object):
 
             """
             if not isinstance(self.__y_data, np.ndarray):
-               raise ValueError('You must call `read` before `label`')
+               raise ValueError('You must call `read` before `label` and '
+               'label_name must be provided')
             else:
                 return self.__y_data
