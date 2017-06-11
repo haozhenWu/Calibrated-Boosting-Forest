e54b78319dc09de5122a715ef0cee376d73c2ff9
haozhenWu <hwu84@wisc.edu>
2017-06-11 17:09:39 -0500

    fpString_to_arry accept custom sep

--- lightchem/utility/util.py
+++ lightchem/utility/util.py
@@ -1,39 +1,48 @@
 import numpy as np
 import pandas as pd
 from sklearn.metrics import auc
+import re
 
-def fpString_to_array(fp_col):
+def fpString_to_array(fp_col, sep = ""):
     """
     Convert fingerprint string into array.
     Parameters:
     -----------
     fp_col: Pandas.Series, each item is a fingerprint string. Ex: 000101,110100
     return fingerprint array.
+    sep: Value used to seperate original value.
     """
     fp_array = []
     for raw_fps in fp_col:
-    # Split k bit fingerprint string into list containing k items.
-    # Then transform list into array so that it can be used for
-    # machine learning/
-        fp_array.append(np.array(list(raw_fps)))
+        # Split k bit fingerprint string into list containing k items.
+        # Then transform list into array so that it can be used for
+        # machine learning
+        if sep == "":
+            fp_array.append(np.array(list(raw_fps)))
+        elif sep == "|":
+            fp_array.append(np.array(re.split("\|",raw_fps)))
+        else:
+            fp_array.append(np.array(re.split(sep,raw_fps)))            
     fp_array = np.array(fp_array)
     fp_array = fp_array.astype(np.float64)
     return fp_array
 
-def array_to_fpString(fp_array):
+def array_to_fpString(fp_array, sep = ""):
     '''
     Convert array back to original fingerprint string format
     Reverse function of fpString_to_array.
     '''
     fpString_list = []
-    k = 0.0
     for array_1d in fp_array:
         fp = ""
-        for i in list(array_1d):
-            fp = fp + str(i)
+        for index,i in enumerate(list(array_1d)):
+            if index == len(list(array_1d))-1:
+                fp = fp + str(i)
+            else:
+                fp = fp + str(i) + sep
         fpString_list.append(fp)
-        k += 1
     return fpString_list
+
 
 # Generate fold index based on each file.
 def reverse_generate_fold_index(whole_df, file_path, fold_num, join_on):
